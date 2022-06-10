a4d669500ae5216f26c1e0e8bf848ed607349915
qpwoeirut <stanleyzhong8@gmail.com>
2022-06-09 19:31:06 -0700

    Don't pause game when 2048 is reached

--- js/game_manager.js
+++ js/game_manager.js
@@ -165,9 +165,6 @@ GameManager.prototype.move = function (direction) {
 
           // Update the score
           self.score += merged.value;
-
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
