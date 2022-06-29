e1a86354363bf647fe921667c67a6744ae7b7d2f
qpwoeirut <stanleyzhong8@gmail.com>
2022-06-29 12:16:44 -0700

    Save settings in localStorage

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
+const STRATEGY_KEY = "2048-aiStrategy"
+const HEURISTIC_KEY = "2048-aiHeuristic"
+const ACTIVE_KEY = "2048-aiOn"
+const PAUSE_TIME_KEY = "2048-pauseTime"
+
 function main(gameManager, playerWorker) {
-    let workerReady = false;
-
     const heuristicContainer = document.getElementById("heuristicContainer");
-
     const updateStrategy = () => {
         const selectedStrategy = document.querySelector('input[name="ai_strategy"]:checked');
         const strategyId = parseInt(selectedStrategy.value);
-        const heuristicId = parseInt(document.querySelector('input[name="ai_heuristic"]:checked').value);
-        playerWorker.postMessage([0, strategyId, heuristicId]);
 
         const hasHeuristic = selectedStrategy.getAttribute("data-heuristic") === "true";
         if (hasHeuristic) {
@@ -15,7 +15,59 @@ function main(gameManager, playerWorker) {
         } else {
             heuristicContainer.classList.add("hidden");
         }
+
+        const selectedHeuristic = document.querySelector('input[name="ai_heuristic"]:checked')
+        const heuristicId = parseInt(selectedHeuristic.value);
+
+        playerWorker.postMessage([0, strategyId, heuristicId]);
+
+        localStorage.setItem(STRATEGY_KEY, selectedStrategy.id)
+        localStorage.setItem(HEURISTIC_KEY, selectedHeuristic.id)
     }
+
+    // initialize settings, read values from localStorage if they exist
+    const aiOn = document.getElementById("aiOn");
+    const aiOff = document.getElementById("aiOff");
+    let aiActive = localStorage.getItem(ACTIVE_KEY) !== "false";
+    if (aiActive) {
+        aiOn.checked = true;
+        aiOff.checked = false;
+    } else {
+        aiOn.checked = false;
+        aiOff.checked = true;
+    }
+    aiOn.onchange = () => {
+        aiActive = aiOn.checked
+        localStorage.setItem(ACTIVE_KEY, aiActive.toString())
+    }
+    aiOff.onchange = () => {
+        aiActive = !aiOff.checked
+        localStorage.setItem(ACTIVE_KEY, aiActive.toString())
+    }
+
+    let pauseTime = parseInt(localStorage.getItem(PAUSE_TIME_KEY));
+    if (isNaN(pauseTime)) pauseTime = 50;
+    const pauseTimeElem = document.getElementById("pauseTime");
+    pauseTimeElem.value = pauseTime.toString()
+    pauseTimeElem.onchange = () => {
+        if (pauseTimeElem.checkValidity()) {
+            pauseTime = parseInt(pauseTimeElem.value);
+            localStorage.setItem(PAUSE_TIME_KEY, pauseTime.toString());
+        }
+    }
+
+    // select previous strategies from saved settings, if they exist
+    document.getElementById(localStorage.getItem(STRATEGY_KEY))?.click();
+    document.getElementById(localStorage.getItem(HEURISTIC_KEY))?.click();
+
+    document.querySelectorAll('input[name="ai_strategy"]').forEach(elem => {
+        elem.onchange = updateStrategy
+    })
+    document.querySelectorAll('input[name="ai_heuristic"]').forEach(elem => {
+        elem.onchange = updateStrategy
+    })
+
+    let workerReady = false;
 
     let nextMove = -1;  // -1 is waiting for response, -2 is waiting for game to restart
     playerWorker.onmessage = (e) => {
@@ -29,31 +81,12 @@ function main(gameManager, playerWorker) {
     }
     playerWorker.onerror = console.error;
 
-    const aiOn = document.getElementById("aiOn");
-    const aiOff = document.getElementById("aiOff");
-    let aiActive = aiOn.checked;
-    aiOn.onchange = function() { aiActive = aiOn.checked }
-    aiOff.onchange = function() { aiActive = !aiOff.checked }
-
-    const pauseTimeElem = document.getElementById("pauseTime");
-    let pauseTime = pauseTimeElem.checkValidity() ? parseInt(pauseTimeElem.value) : 50;
-    pauseTimeElem.onchange = (e) => {
-        if (pauseTimeElem.checkValidity()) {
-            pauseTime = parseInt(pauseTimeElem.value);
-        }
-    }
-
-    document.querySelectorAll('input[name="ai_strategy"]').forEach(elem => {
-        elem.onchange = updateStrategy
-    })
-    document.querySelectorAll('input[name="ai_heuristic"]').forEach(elem => {
-        elem.onchange = updateStrategy
-    })
-
     const MAX_WAIT_TIME = 10000;
     let lastMoveTime = 0;
     const playGame = (timestamp) => {
-        if (aiActive && workerReady && !gameManager.isGameTerminated() && lastMoveTime + pauseTime <= timestamp && nextMove !== -1) {
+        if (aiActive && workerReady && !gameManager.isGameTerminated() &&
+            lastMoveTime + pauseTime <= timestamp && nextMove !== -1)
+        {
             if (0 <= nextMove && nextMove < 4) {
                 gameManager.move(nextMove);
             }
